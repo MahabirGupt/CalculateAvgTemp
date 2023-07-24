a00f4752afc4b372644dd67743253231e783f5b6
Mahabir <mahabirg81@gmail.com>
2023-07-24 15:01:18 +0800

    Calculate avg temperature and number of days above avg temperature

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 //        initializing the scanner class
         Scanner scanner = new Scanner(System.in);
         System.out.print("How many days temperature?");
         int numOfDays = scanner.nextInt();
+//        storing the temperature as array
+        float[] temp = new float[numOfDays];
+        int count = 0;
         double avg = 0;
         double total = 0;
-        for (int i =1; i <=numOfDays; i++){
+//        record temperatures and find average
+        for (int i = 1; i <= numOfDays; i++) {
             System.out.print("Enter temperature for Day " + i + " : ");
-            double temp = scanner.nextFloat();
-
-            total = total + temp;
+            temp[i - 1] = scanner.nextFloat();
+            total = total + temp[i - 1];
         }
         avg = total / numOfDays;
         System.out.println();
         System.out.println("The average temperature is " + avg);
-        int arr[][];
+        for (int i = 0; i < numOfDays; i++) {
+            if (temp[i] > avg) {
+
+                count++;
+            }
+        }
+        System.out.println("There are " + count + " days with temperature more than average temperature");
     }
 }
